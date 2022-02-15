ab338daf39d4aadab001683bd5f96460351a9e58
Lucas Alexandre Zick <lucasalexandrezick@gmail.com>
2022-02-15 15:45:07 -0300

    Started the API working

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
+const pokedex = document.getElementById("pokedex");
+console.log(pokedex)
+
 const fetchPokemon = () => {
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
-    fetch(url)
-        .then(res => {
-            return res.json();
-        })
-        .then ((data) => {
-            console.log(data)
-            const pokemon = {};
-            pokemon['name'] = data.name;
-            pokemon['id'] = data.id;
-            pokemon['image'] = data.sprites['front_default']
-            console.log(pokemon);
-        });
+    const promises = [];
+    for( let i = 1; i < 151; i++){
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+    }
+
+    Promise.all(promises).then( results => {
+        const pokemon = results.map( (data) => ({
+            name: data.name,
+            id: data.id,
+            image: data.sprites['front_default'],
+            type: data.types.map((type) => type.type.name).join(', ')
+        }));
+    displayPokemon(pokemon);
+    });  
+};
+
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon.map ( pokeman => `
+    <li>
+        <img src="${pokeman.image}"/>
+        <h2>${pokeman.id}. ${pokeman.name}</h2>
+        <p>Type: ${pokeman.type}</p>
+    </li>
+    `)
+    pokedex.innerHTML = pokemonHTMLString;
 }
 
-fetchPokemon();
+    fetchPokemon();
